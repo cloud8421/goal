06eaab9a4dea8f3776483e28e5c5888ce7a52f14
Claudio Ortolina <claudio@fullyforged.com>
2018-04-15 11:42:22 +0100

    [BF] Resets test state after every test run

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -5,28 +5,43 @@ module LibSpec
   ) where
 
 import Control.Monad.Logger (runNoLoggingT)
-import Data.Text (Text)
-import Database.Persist.Sqlite (withSqlitePool)
+import Data.Text (Text, unpack)
+import Database.Persist.Sqlite
+  ( SqliteConnectionInfo
+  , mkSqliteConnectionInfo
+  , walEnabled
+  , withSqlitePoolInfo
+  )
+import Lens.Micro (set)
 import Lib (app)
 import Network.Wai (Application)
 import Store (runMigrations)
+import System.Directory (removeFile)
 import Test.Hspec
 import Test.Hspec.Wai
 
 dbPath :: Text
 dbPath = "data/test.db"
 
+connInfo :: SqliteConnectionInfo
+connInfo = set walEnabled False (mkSqliteConnectionInfo dbPath)
+
 application :: IO Application
 application =
-  runNoLoggingT $ withSqlitePool dbPath 1 $ \pool -> return $ app pool
+  runNoLoggingT $ withSqlitePoolInfo connInfo 1 $ \pool -> return $ app pool
 
 migrateDb :: IO ()
 migrateDb =
-  runNoLoggingT $ withSqlitePool dbPath 1 $ \pool -> liftIO $ runMigrations pool
+  runNoLoggingT $
+  withSqlitePoolInfo connInfo 1 $ \pool -> liftIO $ runMigrations pool
+
+removeDbFile :: IO ()
+removeDbFile = removeFile $ unpack dbPath
 
 spec :: Spec
 spec =
   beforeAll_ migrateDb $
+  afterAll_ removeDbFile $
   with application $
   describe "GET /api/projects" $ do
     let req = get "/api/projects"
