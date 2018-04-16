2e204b25c0dbecfeb2683b0e98ad24f16fdd003c
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-04-16 09:00:05 +0100

    [R] Renames type left from code import

--- src/Api/Project.hs
+++ src/Api/Project.hs
@@ -7,9 +7,9 @@ import Database.Persist.Sqlite (Entity)
 import Schema
 import Servant
 
-type GetPatients = "projects" :> Get '[ JSON] [Entity Project]
+type GetProjects = "projects" :> Get '[ JSON] [Entity Project]
 
-type PostPatients
+type PostProjects
    = "projects" :> ReqBody '[ JSON] Project :> PostCreated '[ JSON] (Key Project)
 
-type ProjectApi = GetPatients :<|> PostPatients
+type ProjectApi = GetProjects :<|> PostProjects
