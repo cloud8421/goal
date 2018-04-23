fbeed721e7095aa4ee4779139ed9c4ef444a86f3
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-04-23 17:46:43 +0100

    [F] Adds sample template using bulma elm package

--- frontend/src/View.elm
+++ frontend/src/View.elm
@@ -1,10 +1,44 @@
 module View exposing (..)
 
-import Html exposing (Html, div, text)
+import Bulma.Columns exposing (..)
+import Bulma.Elements exposing (..)
+import Bulma.Layout exposing (..)
+import Bulma.Modifiers exposing (..)
+import Html exposing (Html, main_, text)
 import Types exposing (..)
 
 
 root : Model -> Html Msg
 root model =
-    div []
-        [ model |> toString |> text ]
+    main_ []
+        [ exampleHero model
+        , exampleColumns
+        ]
+
+
+exampleHero : Model -> Html msg
+exampleHero model =
+    hero { heroModifiers | size = Medium, color = Primary }
+        []
+        [ heroBody []
+            [ container []
+                [ title H1 [] [ text "Hero Title" ]
+                , title H2 [] [ model |> toString |> text ]
+                ]
+            ]
+        ]
+
+
+exampleColumns : Html msg
+exampleColumns =
+    section NotSpaced
+        []
+        [ container []
+            [ columns columnsModifiers
+                []
+                [ column columnModifiers [] [ text "First Column" ]
+                , column columnModifiers [] [ text "Second Column" ]
+                , column columnModifiers [] [ text "Third Column" ]
+                ]
+            ]
+        ]
